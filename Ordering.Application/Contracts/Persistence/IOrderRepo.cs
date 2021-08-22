e9d7a8c1a9aec9a8d77f4b72804cffb65b240aa0
Kiran <kiransankar@gmail.com>
2021-08-22 13:06:53 +0530

    Ordering Service with CQRS

---
+++ Ordering.Application/Contracts/Persistence/IOrderRepo.cs
@@ -0,0 +1,11 @@
+﻿using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    public interface IOrderRepo : IAsyncRepo<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
